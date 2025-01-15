fea34d80ffd106df4a7bed7e36706281ea438030
Samy Belazzoug <samy.belazzoug@laplateforme.io>
2025-01-15 15:29:47 +0100

    Job 3 done. Put every single type of comments lines.

---
+++ jour01/job02/index.php
@@ -0,0 +1,10 @@
+<?php
+echo "Hello LaPlateforme!"; //Salut la Plateforme !
+print "Hello LaPlateforme!"; #Et bien le bonjour la Plateforme !
+/*
+Si tu es dev
+Et bien tu peux voir ça
+Sinon
+Beaucoup de gens ne peuvent pas lire ce que tu lis..
+*/
+?>
