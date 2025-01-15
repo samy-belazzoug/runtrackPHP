f358af700076e16b17512809b769817b8db2d148
Samy Belazzoug <samy.belazzoug@laplateforme.io>
2025-01-15 15:17:39 +0100

    job 2 done.

---
+++ jour01/job01/index.php
@@ -0,0 +1,20 @@
+<?php
+$str = "La Plateforme";
+print "<h2>$str</h2><br>";
+
+$str2 = "Vive";
+var_dump($str2);
+$str3 = "!";
+echo "<br>$str2 $str $str3"; 
+
+$val = 6;
+print ("<br> $val <br>");
+$val += 4;
+echo $val;
+
+$myBool = true;
+echo ("<br>$myBool<br>");
+$myBool = false;
+echo $myBool;
+#var_dump($myBool)
+?>
